d4ee8f7e82aa2aab6e5c8b99fa7a6422114b8ddb
Johanserna1023 <93808275+Johanserna1023@users.noreply.github.com>
2024-05-28 19:23:53 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/proyectos/estudios.js
@@ -0,0 +1,17 @@
+export const estudios = [
+  {
+    titulo: "Técnico en desarrollo de software",
+    corporacion: "SENA - Medellín (Antioquia) ",
+    fecha: "01/2020 – 12/2022",
+  },
+  {
+    titulo: "Desarrollador Front-end React Js",
+    corporacion: "Alura Latam - Remoto",
+    fecha: "04/2023 – 09/2023",
+  },
+  {
+    titulo: "Curso profesional de React Js",
+    corporacion: "Código facilito - Remoto",
+    fecha: "04/2023 – 04/2023",
+  },
+];
